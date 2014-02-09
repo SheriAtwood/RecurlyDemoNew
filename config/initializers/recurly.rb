0493f69d581d07aa162f36a3cebceee2780e1ee1
SupportPay <sheri.atwood@supportpay.com>
2014-02-09 00:42:33 -0800

    added views

---
+++ config/initializers/recurly.rb
@@ -0,0 +1,4 @@
+Recurly.api_key = ENV['3d301163be884aa5935baa1b76bc50a7']
+Recurly.js.private_key = ENV['a26316e92322494689c59a1d65b72e5b']
+RECURLY_SUBDOMAIN = ENV['supportpay']
+# Recurly.default_currency = 'USD'
