040894ae8aa929177e59ff377eb531910c6a8caa
SupportPay <sheri.atwood@supportpay.com>
2014-02-09 00:54:37 -0800

    changed app_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,3 @@
 class ApplicationController < ActionController::Base
-  require "omniauth"
-	require "omniauth-salesforce"
-end
 
- use OmniAuth::Builder do
-provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
-end
-
- get '/authenticate' do
-redirect "/auth/salesforce"
-end
-get '/auth/salesforce/callback' do
-logger.info "#{env["omniauth.auth"]["extra"]["display_name"]}
-authenticated"
-env["omniauth.auth"]["extra"]["display_name"]
-end
-get '/auth/failure' do
-params[:message]
-end
-get '/unauthenticate' do
-session.clear
-'Goodbye - you are now logged out'
 end
