2dd9ffacd015d056ed199185b8969d62026365c3
SupportPay <sheri.atwood@supportpay.com>
2014-02-08 23:56:35 -0800

    oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,24 @@
-# Copyright (c) 2011, salesforce.com, inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without modification, are permitted provided
-# that the following conditions are met:
-#
-#    Redistributions of source code must retain the above copyright notice, this list of conditions and the
-#    following disclaimer.
-#
-#    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
-#    the following disclaimer in the documentation and/or other materials provided with the distribution.
-#
-#    Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or
-#    promote products derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  require "omniauth"
+	require "omniauth-salesforce"
+end
+
+ use OmniAuth::Builder do
+provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
+end
+
+ get '/authenticate' do
+redirect "/auth/salesforce"
+end
+get '/auth/salesforce/callback' do
+logger.info "#{env["omniauth.auth"]["extra"]["display_name"]}
+authenticated"
+env["omniauth.auth"]["extra"]["display_name"]
+end
+get '/auth/failure' do
+params[:message]
+end
+get '/unauthenticate' do
+session.clear
+'Goodbye - you are now logged out'
 end
