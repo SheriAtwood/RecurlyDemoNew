62507881ee41e5e5a99b4be00424a09057a3d745
SupportPay <sheri.atwood@supportpay.com>
2014-02-10 02:04:26 -0800

    changes with demo files

--- Gemfile
+++ Gemfile
@@ -1,6 +1,15 @@
 source 'https://rubygems.org'
 
-gemspec
+gem 'rails', '3.2.7'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'json'
+gem 'recurly', '~> 2.2.1'
+gem 'bcrypt-ruby', '3.0.1'
+gem 'bundler', '1.5.3'
+gem 'therubyracer'
 
 group :development do
   gem 'nokogiri',      '~> 1.5.0', :group     => :test
@@ -11,21 +20,25 @@ group :development do
   gem 'rubysl',                    :platforms => :rbx
   gem 'rubysl-resolv', '~> 2.0',   :platforms => :rbx
   gem 'racc',                      :platforms => :rbx
+	gem 'sqlite3'
+	gem 'rspec-rails', '2.10.0'
+	gem 'guard-rspec', '0.5.5'
+end
 
+group :production do
+	gem 'pg' , '0.12.2'
 end
 
 gem 'rails_12factor', group: :production
-gem 'rails', '3.2.12'
-gem 'recurly'
-gem 'bundler', '1.5.3'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-gem 'therubyracer'
+
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '1.2.3'
 end
 
 gem 'jquery-rails'
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,16 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
-
-  def index
-    authorize! :index, @user, :message => 'Not authorized as an administrator.'
-    @users = User.all
+  def new
+  	@user = User.new
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def update
-    authorize! :update, @user, :message => 'Not authorized as an administrator.'
-    @user = User.find(params[:id])
-    role = Role.find(params[:user][:role_ids]) unless params[:user][:role_ids].nil?
-    params[:user] = params[:user].except(:role_ids)
-    if @user.update_attributes(params[:user])
-      @user.update_plan(role) unless role.nil?
-      redirect_to users_path, :notice => "User updated."
-    else
-      redirect_to users_path, :alert => "Unable to update user."
-    end
-  end
-
-  def destroy
-    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
-    user = User.find(params[:id])
-    unless user == current_user
-      user.destroy
-      redirect_to users_path, :notice => "User deleted."
-    else
-      redirect_to users_path, :notice => "Can't delete yourself."
-    end
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		# Handle a successful save
+      # Create a user's Recurly account to associate this with
+      redirect_to new_subscription_path
+  	else
+  		render 'new'
+  	end
   end
 end
